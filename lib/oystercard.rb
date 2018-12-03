f9f88533b8b04d8a3460fa9c9c01c30d52dfcbe5
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-12-03 17:17:04 +0000

    added raising error function to Oystercard class if attempting to topup over £90 limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,14 @@
 class Oystercard
 
   attr_reader :balance
-  BALANCE_LIMIT = 90
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
-    "Error: cannot exceed £90 limit"
+    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,19 +5,23 @@ describe Oystercard do
   let(:oyster) { Oystercard.new }
 
   it "has a balance of zero" do
-    expect(subject.balance).to eq(0)
+   expect(subject.balance).to eq(0)
   end
 
   it "the balance can be topped up" do
-    expect(subject).to respond_to(:top_up).with(1).argument
+   expect(subject).to respond_to(:top_up).with(1).argument
   end
 
   it "balance increased with #top_up" do
-    expect { subject.top_up(10) }.to change { oyster.balance }. by 10
+   expect { subject.top_up(10) }.to change { subject.balance }. by 10
   end
 
-  it "has a balance limit of 90" do
-    message = "Error: cannot exceed £90 limit"
-    expect { oyster.top_up(91) }.to eq message
+  
+  it 'raises an error if the maximum balance is exceeded' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
   end
+
+
 end
