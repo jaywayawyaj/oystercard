8b22a082aa41953cede577a1bca2a18291d8baf3
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2018-12-03 16:04:15 +0000

    added top_up method which increases balance amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,13 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it "the balance can be topped up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "balance increased with #top_up" do
+    subject.top_up(10)
+    expect(subject.balance).to eql(10)
+  end
+
 end
